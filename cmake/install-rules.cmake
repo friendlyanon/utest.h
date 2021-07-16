c24593a29bf3d4f1e79ba1c1be86fa9e9cb538d3
friendlyanon <friendlyanon@users.noreply.github.com>
2021-07-17 00:04:34 +0200

    Update install rules to latest cmake-init's

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -3,10 +3,13 @@ if(PROJECT_IS_TOP_LEVEL)
 endif()
 
 # Project is configured with no languages, so tell GNUInstallDirs the lib dir
-set(CMAKE_INSTALL_LIBDIR lib CACHE PATH "Object code libraries (lib)")
+set(CMAKE_INSTALL_LIBDIR lib CACHE PATH "")
 
 include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
+
+# find_package(<package>) call for consumers to find this project
+set(package utest)
 
 install(
     DIRECTORY include/
@@ -21,15 +24,15 @@ install(
 )
 
 write_basic_package_version_file(
-    utestConfigVersion.cmake
+    "${package}ConfigVersion.cmake"
     COMPATIBILITY SameMajorVersion
     ARCH_INDEPENDENT
 )
 
 # Allow package maintainers to freely override the path for the configs
 set(
-    utest_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/utest"
-    CACHE STRING "CMake package config location relative to the install prefix"
+    utest_INSTALL_CMAKEDIR "${CMAKE_INSTALL_DATADIR}/${package}"
+    CACHE PATH "CMake package config location relative to the install prefix"
 )
 mark_as_advanced(utest_INSTALL_CMAKEDIR)
 
@@ -40,9 +43,14 @@ install(
 )
 
 install(
-    FILES
-    cmake/utestConfig.cmake
-    "${PROJECT_BINARY_DIR}/utestConfigVersion.cmake"
+    FILES cmake/install-config.cmake
+    DESTINATION "${utest_INSTALL_CMAKEDIR}"
+    RENAME "${package}Config.cmake"
+    COMPONENT utest_Development
+)
+
+install(
+    FILES "${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake"
     DESTINATION "${utest_INSTALL_CMAKEDIR}"
     COMPONENT utest_Development
 )
